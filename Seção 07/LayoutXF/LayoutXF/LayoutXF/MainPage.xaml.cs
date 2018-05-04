682e31c2ce319c166ab32c3e8371e48993d99028
Bruno Tomaz <brunohapki@gmail.com>
2018-05-04 15:21:16 -0300

    Layout - StackLayout

---
+++ Seção 07/LayoutXF/LayoutXF/LayoutXF/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LayoutXF
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+		}
+
+        private void GoPaginaStack(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new LayoutXF.Layouts.Stack.StackPage());
+        }
+
+        private void GoPaginaGrid(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new LayoutXF.Layouts.Grid.GridPage());
+        }
+
+        private void GoPaginaAbsolute(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new LayoutXF.Layouts.Absolute.AbsolutePage());
+        }
+
+        private void GoPaginaRelative(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new LayoutXF.Layouts.Relative.RelativePage());
+        }
+
+        private void GoPaginaScroll(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new LayoutXF.Layouts.Scroll.ScrollPage());
+        }
+    }
+}
